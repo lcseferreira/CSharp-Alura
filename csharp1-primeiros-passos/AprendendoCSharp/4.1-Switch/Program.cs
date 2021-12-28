8f397dff64c8699843d79d7d04997ae4e117c408
Lucas Ferreira <lcseferreira@hotmail.com>
2021-12-28 10:57:47 -0300

    Aula 4.2 - Condicionais: switch case

---
+++ csharp1-primeiros-passos/AprendendoCSharp/4.1-Switch/Program.cs
@@ -0,0 +1,31 @@
+﻿// Usando switch case
+
+int diaDaSemana = 9;
+
+switch (diaDaSemana)
+{
+    case 1:
+        Console.WriteLine("Domingo");
+        break;
+    case 2:
+        Console.WriteLine("Segunda");
+        break;
+    case 3:
+        Console.WriteLine("Terça");
+        break;
+    case 4:
+        Console.WriteLine("Quarta");
+        break;
+    case 5:
+        Console.WriteLine("Quinta");
+        break;
+    case 6:
+        Console.WriteLine("Sexta");
+        break;
+    case 7:
+        Console.WriteLine("Sábado");
+        break;
+    default:
+        Console.WriteLine("Entrada inválida");
+        break;
+}
