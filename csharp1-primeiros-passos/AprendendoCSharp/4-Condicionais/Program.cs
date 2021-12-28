dd699fadee427b7de5d3247f8543fadeb2783c26
Lucas Ferreira <lcseferreira@hotmail.com>
2021-12-28 10:39:05 -0300

    Aula 4 - Condicionais

---
+++ csharp1-primeiros-passos/AprendendoCSharp/4-Condicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Executando Projeto 4 - Condicionais");
+
+int idadeLucas = 23;
+
+if (idadeLucas >= 18)
+{
+    Console.WriteLine($"Idade: {idadeLucas}. Pode entrar.");
+}
+else if (idadeLucas >= 16)
+{
+    Console.WriteLine($"Idade: {idadeLucas}. Pode entrar acompanhado com os pais.");
+}
+else
+{
+    Console.WriteLine($"Idade: {idadeLucas}. Não pode entrar.");
+}
+
+// Escopo
+int idadeMarcos = 17;
+bool acompanhado = false;
+string mensagemAdicional;
+
+if (idadeMarcos >= 18 || acompanhado)
+{
+    if (acompanhado)
+    {
+        mensagemAdicional = "está acompanhado.";
+    }
+    else
+    {
+        mensagemAdicional = "não está acompanhado.";
+    }
+    Console.WriteLine($"Marcos tem {idadeMarcos} anos e {mensagemAdicional}. Pode entrar.");
+}
+else
+{
+    Console.WriteLine("Marcos não pode entrar.");
+}
