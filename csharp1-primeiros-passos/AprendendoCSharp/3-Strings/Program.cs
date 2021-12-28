20a01f63362f238bbf07ff6aa6feabe793fb9d72
Lucas Ferreira <lcseferreira@hotmail.com>
2021-12-28 09:55:51 -0300

    Aula 3 - Strings

---
+++ csharp1-primeiros-passos/AprendendoCSharp/3-Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Executando o Projeto 3 - Strings");
+
+// character ' '
+char umaLetra = 'L';
+Console.WriteLine($"umaLetra = 'L' = {umaLetra}");
+
+umaLetra = (char)65;
+Console.WriteLine($"(char)65 = {umaLetra}");
+
+// string
+string texto = "Esse é um texto usando string";
+Console.WriteLine(texto);
+
+string testeLinhas1 = "Cursos:" +
+    "\n- Java" +
+    "\n- Python" +
+    "\n- C#";
+
+string testeLinhas2 = 
+@"Cursos:
+- Java
+- Python
+- C#";
+
+Console.WriteLine(testeLinhas1);
+Console.WriteLine(testeLinhas2);
+
