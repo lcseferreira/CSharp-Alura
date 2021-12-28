442c45010397733e8ac47d9964cb19d8a440b7ee
Lucas Ferreira <lcseferreira@hotmail.com>
2021-12-28 13:04:28 -0300

    Aula 6 e 7 - While e For

---
+++ csharp1-primeiros-passos/AprendendoCSharp/5-CalculaPoupanca/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Projeto Calcula Poupança");
+
+double valorInvestido = 1000;
+
+// 0.0036 = 0.36%
+valorInvestido += valorInvestido * 0.36 / 100;
+
+Console.WriteLine($"Após 1 mês, você terá R$ {valorInvestido.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine();
+
+// Usando o while para calcular a variação da poupança de acordo com os meses
+int contadorMes = 1;
+
+while (contadorMes <= 12)
+{
+    valorInvestido += valorInvestido * 0.36 / 100;
+    Console.WriteLine($"Após {contadorMes} mês/meses, você terá R$ {valorInvestido.ToString("F2", CultureInfo.InvariantCulture)}");
+    // contadorMes = contadorMes + 1;
+    // contadorMes += 1;
+    contadorMes++;
+}
---
+++ csharp1-primeiros-passos/AprendendoCSharp/6-For/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Globalization;
+
+// Usando laço de repetição FOR
+double valorInvestido = 1000;
+double fatorRendimento = 1.0036;
+
+for (int mes = 1; mes <= 12; mes++)
+{
+    valorInvestido *= fatorRendimento;
+    Console.WriteLine($"Mês: {mes} - Valor: R${valorInvestido.ToString("F2", CultureInfo.InvariantCulture)}");
+}
+
+// Usando laço de repetição FOR encadeado
+valorInvestido = 2000;
+
+
+// O tempo do investimento será de 5 anos
+for (int ano = 1; ano <= 5; ano++)
+{
+    for (int mes = 1; mes <= 12; mes++)
+    {
+        valorInvestido *= fatorRendimento;
+        Console.WriteLine($"Ano: {ano} - Mês: {mes} - Valor: R${valorInvestido.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+    fatorRendimento += 0.0010;
+}
+
+Console.WriteLine();
+
+// Transformando WHILE em FOR
+int contador = 1;
+
+Console.WriteLine("WHILE");
+while (contador <= 10)
+{
+    Console.WriteLine(contador);
+    contador++;
+}
+
+Console.WriteLine();
+
+Console.WriteLine("FOR");
+for (int c1 = 1; c1 <= 10; c1++)
+{
+    Console.WriteLine(c1);
+}
+
+Console.WriteLine();
+
+// O uso do BREAK
+for (int linha = 0; linha <= 10; linha++)
+{
+    for (int coluna = 0; coluna <= 10; coluna++)
+    {
+        Console.Write("*");
+        if (coluna >= linha) break;
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// Forma alternativa sem o BREAK
+for (int linha = 0; linha <= 10; linha++)
+{
+    for (int coluna = 0; coluna <= linha; coluna++) Console.Write("*");
+    Console.WriteLine();
+}
+
+// Exercício com FOR
+for (int x = 0; x < 5; x++)
+{
+    for (int y = 0; y < 5; y++)
+    {
+        if (x < y) break;
+        Console.Write(y + 1);
+    }
+    Console.WriteLine();
+}
