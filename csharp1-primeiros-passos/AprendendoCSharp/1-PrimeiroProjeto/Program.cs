b841451f7ad4c0c252ca5c7b3be254d967f5e404
Lucas Ferreira <lcseferreira@hotmail.com>
2021-12-28 08:55:49 -0300

    Aula 1 - Primeiro Projeto

---
+++ csharp1-primeiros-passos/AprendendoCSharp/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo. Projeto no Visual Studio.");
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
