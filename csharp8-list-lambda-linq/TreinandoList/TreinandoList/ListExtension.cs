d7c20cdb8a4cd8a5ed184de8ed6238a715818cbb
Lucas <lcseferreira@hotmail.com>
2022-02-15 08:02:48 -0300

    C# parte 8 - Métodos de extensão

---
+++ csharp8-list-lambda-linq/TreinandoList/TreinandoList/ListExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinandoList
+{
+    // Extensões da classe List
+    public static class ListExtension
+    {
+        // Método de extensão
+        public static void AdicionarVarios(this List<int> lista, params int[] itens)
+        {
+            foreach(var item in itens)
+            {
+                lista.Add(item);
+            } 
+        }
+    }
+}
---
+++ csharp8-list-lambda-linq/TreinandoList/TreinandoList/Program.cs
@@ -0,0 +1,40 @@
+﻿/*** Treinando List<> no .NET ***/
+
+// Criando uma lista - List<GENERIC>
+using TreinandoList;
+
+List<int> ages = new List<int>();
+
+// Adicionando itens na lista 
+ages.Add(23);
+ages.Add(32);
+ages.Add(50);
+
+// for em List
+for (int i = 0; i < ages.Count; i++)
+{
+    Console.WriteLine($"Age: {ages[i]}");
+}
+
+Console.WriteLine();
+
+// foreach em List
+foreach(int age in ages) {
+    Console.WriteLine($"Age: {age}");
+}
+
+// Adicionando vários itens em uma List
+ages.AddRange(new int[] { 45, 55, 23, 9 });
+
+// Utilizando um método de extensão
+ages.AdicionarVarios(14, 20, 33, 12);
+
+Console.WriteLine();
+
+foreach (int age in ages)
+{
+    Console.WriteLine($"Age: {age}");
+}
+
+
+
