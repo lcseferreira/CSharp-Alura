6501d1dfcd3eb33c1ce033ac5e4c24a64dde4254
Lucas <lcseferreira@hotmail.com>
2022-02-17 10:50:22 -0300

    C# parte 9 - StremWriter, Create e CreateNew

---
+++ csharp9-io/IO/IO/CriandoArquivo.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        // Caminho e nome do arquivo que vamos criar
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+
+        {
+            {
+                var contaComoString = "456,12345,7728.98,Lucas Ferreira";
+
+                var encoding = Encoding.UTF8;
+
+                // Obtendo os bytes da string
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+
+    static void CriarArquivoWriter()
+    {
+        // Caminho e nome do arquivo que vamos criar
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoArquivo, Encoding.UTF8))
+        {
+            escritor.Write("456,12345,7728.98,Lucas Ferreira");
+        }
+    }
+}
--- csharp9-io/IO/IO/Program.cs
+++ csharp9-io/IO/IO/Program.cs
@@ -4,53 +4,8 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoArquivo = "Contas.txt";
-
-        using (var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open)) // Abrindo o arquivo
-        using (var leitorStream = new StreamReader(fluxoArquivo)) // Leitor de Stream)
-        {
-            while (!leitorStream.EndOfStream)
-            {
-                // Lendo linha por linha
-                var leitorLinha = leitorStream.ReadLine();
-
-                // Convertendo para ContaCorrente
-                var contaCorrente = ConverterStringParaContaCorrente(leitorLinha);
-
-                Console.WriteLine($"Titular: {contaCorrente.Titular.Nome} - Agência: {contaCorrente.Agencia} - Conta: {contaCorrente.Numero} - " +
-                    $"Saldo: R$ {contaCorrente.Saldo}");
-            }
-        }
-        // Lendo até o final - Usar com cuidado
-        //var leitorFim = leitorStream.ReadToEnd();
-        //Console.WriteLine(leitorFim);
+        //CriarArquivo();
+        CriarArquivoWriter();
+        Console.WriteLine("Aplicação finalizada!");
     }
-
-    static ContaCorrente ConverterStringParaContaCorrente(string linha)
-    {
-        var campos = linha.Split(',');
-
-        var agencia = campos[0];
-        var numero = campos[1];
-        var saldo = campos[2].Replace('.', ',');
-        var nomeTitular = campos[3];
-
-        var agenciaInt = int.Parse(agencia);
-        var numeroInt = int.Parse(numero);
-
-        var saldoDouble = double.Parse(saldo);
-
-        var titular = new Cliente();
-        titular.Nome = nomeTitular;
-
-        var resultado = new ContaCorrente(agenciaInt, numeroInt);
-
-        resultado.Depositar(saldoDouble);
-        resultado.Titular = titular;
-
-        return resultado;
-    }
-
-
 }
-
