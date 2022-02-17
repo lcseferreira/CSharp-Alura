1e37dcc880b7f4b2813d7fcdbff962e76a326f18
Lucas <lcseferreira@hotmail.com>
2022-02-17 08:36:08 -0300

    C# parte 9 - Método Close e using

--- csharp9-io/IO/IO/Program.cs
+++ csharp9-io/IO/IO/Program.cs
@@ -5,19 +5,28 @@ using System.Text;
 
 var enderecoArquivo = "contas.txt";
 
-// FileStream(endereco, ação)
-var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
-
-// buffer = usado para guardar informações temporárias
-var buffer = new byte[1024]; // 1 kb
-var numeroBytesLidos = -1;
-
-while (numeroBytesLidos != 0) // Quando for 0, significa que acabou a leitura
+// FileStream(endereco, ação) - Depois que usar, será chamado o Dispose
+using (var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open))
 {
-    // Read(array de bytes para gravar informações, índice de preenchimento, quantidade de bytes do array) 
-    numeroBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
-    EscreverBuffer(buffer);
+    // buffer = usado para guardar informações temporárias
+    var buffer = new byte[1024]; // 1 kb
+    var numeroBytesLidos = -1;
+
+    while (numeroBytesLidos != 0) // Quando for 0, significa que acabou a leitura
+    {
+        // Read(array de bytes para gravar informações, índice de preenchimento, quantidade de bytes do array) 
+        numeroBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+        EscreverBuffer(buffer);
+    }
+
+    // Fechando o arquivo
+    //fluxoArquivo.Close();
 }
+
+
+
+
+
 
 
 
