20203b9956254ebbdd2e426286189f27f299128d
Lucas <lcseferreira@hotmail.com>
2022-02-17 07:24:33 -0300

    C# parte 9 - IO, Buffer e FileStream

---
+++ csharp9-io/IO/IO/Models/ContaCorrente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO.Models
+{
+    public class ContaCorrente
+    {
+        public int Numero { get; set; }
+        public int Agencia { get; set; }
+        public double Saldo { get; private set; }
+        public Cliente Cliente { get; set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("Valor do depósito deve ser maior que 0", nameof(valor));
+            }
+
+            Saldo += valor;
+        }
+    }
+}
---
+++ csharp9-io/IO/IO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO; // IO = Input Output (Entrada e Saída)
+
+/*** Criando um FileStream ***/
+
+var enderecoArquivo = "contas.txt";
+
+// FileStream(endereco, ação)
+var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+
+// buffer = usado para guardar informações temporárias
+var buffer = new byte[1024]; // 1 kb
+var numeroBytesLidos = -1;
+
+while (numeroBytesLidos != 0) // Quando for 0, significa que acabou a leitura
+{
+    // Read(array de bytes para gravar informações, índice de preenchimento, quantidade de bytes do array) 
+    numeroBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+    EscreverBuffer(buffer);
+}
+
+
+
+
+
+// Classe estática para escrever meus bytes
+static void EscreverBuffer(byte[] buffer)
+{
+    foreach (var myByte in buffer)
+    {
+        Console.Write($"{myByte} ");
+    }
+}
